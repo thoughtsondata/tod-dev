985bacadc8a559b26b595f78d1e24b632d0b9492
Zarek <zarekbusiness@gmail.com>
2022-04-30 17:24:11 -0400

    Site settings updated

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,3 +31,15 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = False
+SUMMARY_MAX_LENGTH = 50
+
+ARCHIVES_URL = "archives.html"
+ARCHIVES_SAVE_AS = 'archives.html'
+ARTICLE_URL = 'articles/{slug}.html'
+ARTICLE_SAVE_AS = 'articles/{slug}.html'
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+TAGS_URL = 'tag/{slug}.html'
