8ec4d63099d20ac667f6b9eb2eaf76d1a66a5d1e
Zarek <zarekbusiness@gmail.com>
2022-04-30 17:03:47 -0400

    Added theme

---
+++ content/pages/about.md
@@ -0,0 +1,6 @@
+Title: About Me
+Date: 2020-05-27 10:20
+Slug: about
+Authors: Frank Corso
+
+Hey there! My name is Frank Corso and it's nice to meet you.
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = 'Ivey'
+SITENAME = 'Thoughts on Data'
+SITEURL = ''
+
+PATH = 'content'
+THEME = './themes/voce/'                          # make sure path points to folder where you cloned the theme
+DEFAULT_DATE_FORMAT = "%b %d, %Y"    
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
