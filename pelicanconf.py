96da9bbc089ba567c4e69670d4eb4457ffea93e8
Zarek <zarekbusiness@gmail.com>
2022-04-30 22:54:17 -0400

    Setting updated

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,7 +25,22 @@ DISPLAY_PAGES_ON_MENU = True
 SOCIAL = (('Twitter', 'https://twitter.com/thoughtsondata'),
           ('Email', 'mailto:hello@thoughtsondata.dev'),)
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 8
+RELATIVE_URLS = True
+DELETE_OUTPUT_DIRECTORY = True
+# OUTPUT_RETENTION = [".git"]
+
+DISPLAY_CATEGORIES_ON_MENU = True
+DISPLAY_PAGES_ON_MENU = True
+SUMMARY_MAX_LENGTH = 50
+
+ARCHIVES_URL = "archives.html"
+ARCHIVES_SAVE_AS = 'archives.html'
+ARTICLE_URL = 'articles/{slug}.html'
+ARTICLE_SAVE_AS = 'articles/{slug}.html'
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+TAGS_URL = 'tag/{slug}.html'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
